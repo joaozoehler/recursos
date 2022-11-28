0943eb9dc9e43b62e0ac9caa838d71176503f819
joaozoehler <89044655+joaozoehler@users.noreply.github.com>
2022-11-28 18:00:49 -0300

    Update - v. 1.0.0 (fuso Brazil/Sao Paulo GMT (-3))

--- get_time.py
+++ get_time.py
@@ -1,3 +1,4 @@
 def get_time():
-    from datetime import datetime, timezone
-    return datetime.now(timezone.utc).strftime("%Y_%m_%d_%H_%M_%S")
+    from datetime import datetime
+    from pytz import timezone
+    return datetime.now().astimezone(timezone("America/Sao_Paulo")).strftime("%Y_%m_%d_%H_%M_%S")
